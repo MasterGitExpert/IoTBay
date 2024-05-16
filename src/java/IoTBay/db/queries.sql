0f3fe699993019c2d976a16c2a619286f1d473ea
Krish <krish.patel-1@student.uts.edu.au>
2024-05-16 18:44:23 +1000

    DBManager fix

--- src/java/IoTBay/db/queries.sql
+++ src/java/IoTBay/db/queries.sql
@@ -6,32 +6,32 @@
 
 -- Create
 
-INSERT INTO User(first_name, last_name, email, pass, phone, registration_date, access) 
+INSERT INTO Users(first_name, last_name, email, pass, phone, registration_date, access) 
 VALUES ('coAdmin', 'coAdmin', 'co_admin@mail.com', 'co2admin143', '0123 456 789', '2020-1-1 00:00:00', 'ADMIN');
 
-INSERT INTO UserAccessLog (user_id, login_datetime, logout_datetime)
+INSERT INTO UsersAccessLog (user_id, login_datetime, logout_datetime)
 VALUES (1, '2020-1-1 00:00:00', '2020-1-1 01:00:00');
 
 
 -- Read
 
-SELECT * FROM User
+SELECT * FROM Users
 WHERE email = 'staff@mail.com';
 
-SELECT * FROM User
+SELECT * FROM Users
 WHERE email = 'admin@mail.com';
 
-SELECT COUNT(*) as user_count FROM User
+SELECT COUNT(*) as user_count FROM Users
 WHERE email = 'admin@mail.com'
 AND pass = 'iotadmin';
 
-SELECT * FROM User
+SELECT * FROM Users
 WHERE email = 'un3@mail.com'
 AND pass = 'password';
 
 SELECT * FROM UserAccessLog
 WHERE user_id = (
-    SELECT user_id FROM User
+    SELECT user_id FROM Users
     WHERE email = 'admin@mail.com'
     )
 ORDER BY DESC;
@@ -39,7 +39,7 @@ ORDER BY DESC;
 
 -- Update
 
-UPDATE User
+UPDATE Users
 SET first_name = 'Admin',
 last_name = 'Admin',
 email = 'admin@mail.com',
@@ -47,7 +47,7 @@ pass = 'iotadmin',
 phone = '0123 456 789',
 WHERE email = 'admin@mail.com';
 
-UPDATE User
+UPDATE Users
 SET first_name = 'Admin',
 last_name = 'Admin',
 email = 'admin@mail.com',
@@ -58,10 +58,10 @@ WHERE email = 'admin@mail.com';
 
 -- Delete
 
-DELETE FROM User
+DELETE FROM Users
 WHERE email = 'un5@gmail.com';
 
-DELETE FROM User
+DELETE FROM Users
 WHERE email = 'staff3@gmail.com';
 
 
